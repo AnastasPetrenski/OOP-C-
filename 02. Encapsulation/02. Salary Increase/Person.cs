57800c600c48874ef324d5902d8edf9eae5ea2de
AnastasPetrenski <hloro83@gmail.com>
2020-11-01 13:56:09 +0200

    Encapsulation

---
+++ 02. Encapsulation/02. Salary Increase/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string secondName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get;  }
+        public int Age { get; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";           
+        }
+    }
+}
