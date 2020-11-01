e5bf6e97ac1bedef747f84da3823a143f08a4084
AnastasPetrenski <hloro83@gmail.com>
2020-11-01 08:19:32 +0200

    Encapsulation

---
+++ 02. Encapsulation/01. Sort Persons by Name and Age/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string secondName;
+        private int age;
+
+        public Person(string firstName, string secondName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = LastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
