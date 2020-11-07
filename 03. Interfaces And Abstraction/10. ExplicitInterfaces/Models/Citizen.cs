fde48e28f904ea6b91d9e87c3ee4b81a41c6f3a7
AnastasPetrenski <hloro83@gmail.com>
2020-11-07 20:05:33 +0200

    InterfacesAndAbstraction

---
+++ 03. Interfaces And Abstraction/10. ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using ExplicitInterfaces.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, string age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string Age { get; private set; }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name} ";
+        }
+
+        string IPerson.GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ 03. Interfaces And Abstraction/10. ExplicitInterfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExplicitInterfaces.Interfaces;
+using ExplicitInterfaces.Models;
+using System;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var inputArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = inputArgs[0];
+                string country = inputArgs[1];
+                string age = inputArgs[2];
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson personGetName = citizen;
+                IResident residentGetName = citizen;
+                Console.WriteLine(personGetName.GetName());
+                Console.WriteLine(residentGetName.GetName());
+            }
+        }
+    }
+}
